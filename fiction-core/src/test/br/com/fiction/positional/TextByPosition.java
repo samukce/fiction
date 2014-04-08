8edca99cc9d2a7ee431d301e783457bc8e91899f
Samuel Pereira <samuel.p.araujo@gmail.com>
2014-04-07 22:41:54 -0300

    Project created

---
+++ fiction-core/src/test/br/com/fiction/positional/TextByPosition.java
@@ -0,0 +1,14 @@
+package test.br.com.fiction.positional;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextByPosition {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
