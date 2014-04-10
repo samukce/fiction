55cff3ee372653415e8178b4edfb2c64e9abcffb
Samuel Pereira <samuel.p.araujo@gmail.com>
2014-04-09 21:50:10 -0300

    no message

--- fiction-core/src/test/br/com/fiction/positional/TextByPosition.java
+++ fiction-core/src/test/br/com/fiction/positional/TextByPosition.java
@@ -14,6 +14,14 @@ public class TextByPosition {
         assertEquals("123456", mockPositional.getTrack().getValue());
         assertEquals("John Foo", mockPositional.getName().getValue());
 	}
+	
+	@Test
+	public void shouldTruncateIfTextGreaterThanTotalConfigured(){
+        MockPositional mockPositional = new MockPositional("123456John Foooooooo");
+        mockPositional.synchronizeTextToObject();
 
+        assertEquals("123456", mockPositional.getTrack().getValue());
+        assertEquals("John Foo", mockPositional.getName().getValue());
+	}
 }
 
