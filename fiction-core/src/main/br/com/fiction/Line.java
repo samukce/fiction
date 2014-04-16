499798c3209165ee10a73a499a0795d42cb178bb
samuel pereira de araujo <samukce@gmail.com>
2014-04-15 22:26:43 -0300

    Starting documenting of code source.

--- fiction-core/src/main/br/com/fiction/Line.java
+++ fiction-core/src/main/br/com/fiction/Line.java
@@ -33,6 +33,20 @@ public abstract class Line {
 		objectToText();
 	}
 	
+	/**
+     * Register your fields:
+     * <p>Example: 
+	 * <pre class="code"><code class="java">
+	 * 
+	 * public class ExamplePositional extends PositionalLine {
+	 *    private PositionalField track;
+	 *
+	 *    protected void registerFields() {
+	 *       this.field = new PositionalField(this, 1, 10, false);
+	 *    }
+	 * }
+	 * </code></pre>
+     */
 	protected abstract void registerFields(); 
 	
 	protected abstract void objectToText(); 
